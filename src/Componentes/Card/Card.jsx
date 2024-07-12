df51af3975f82f956fe42e987a58c940795b610b
ikkitonga <mcgaston@gmail.com>
2024-07-12 19:12:38 -0300

    estilos al card

--- src/Componentes/Card/Card.jsx
+++ src/Componentes/Card/Card.jsx
@@ -6,29 +6,33 @@ import { VideoContext } from "../../VideoContext"
 
 const Card = ({ imagen, descripcion, expansion, id, link }) => {
 
-// funcion deleteVideo traida del videoContext
+    // funcion deleteVideo traida del videoContext
     const { deleteVideo } = useContext(VideoContext)
 
 
-// funcion para manejar el borrado del video
+    // funcion para manejar el borrado del video
     const handleDelete = () => {
-        console.log("el video se borro") 
+        console.log("el video se borro")
         deleteVideo(id)
     }
 
     return <div className="contenedor__card">
         <a href={link} target="_blank">
+
             <img src={imagen} alt={expansion} className="imagen__card" />
             <h3>{descripcion}</h3>
         </a>
+
+
         <div className="contenedor__button">
             <button className="card__btn" onClick={handleDelete}>Borrar</button>
             {/* <button className="card__btn">Editar</button> */}
         </div>
+
         {/* <div className="contenedor__favorito">
 
         </div> */}
-    </div>
+    </div >
 }
 
 export default Card 
