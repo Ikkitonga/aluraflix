7a76a5316c322210e85a2d2f9e686028393c71c4
ikkitonga <mcgaston@gmail.com>
2024-07-14 11:30:46 -0300

    cambio en la direccion del logo

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 const Footer = () => {
     return <footer>
         <div className="footer__image">
-            <img src="../public/Logo/logo.png" alt="Logo"></img>
+            <img src="https://aluraflix-jade.vercel.app/static/media/Logo.5e7b3578.png" alt="Logo"></img>
         </div>
         <div className="footer__text">
             <p>Proyecto para curso de ALURA-ORACLE</p>
--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 
 const Navbar = () => {
     return <nav className="navbar">
-        <img src="../public/Logo/logo.png" alt="Logo"></img>
+        <img src="https://aluraflix-jade.vercel.app/static/media/Logo.5e7b3578.png" alt="Logo"></img>
 
         <div className="navbar__botones">
          <a href="/"><button className="boton">HOME</button></a> 
