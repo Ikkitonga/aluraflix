e9da4391ed018078ce044f75abc9b389aa33e20a
ikkitonga <mcgaston@gmail.com>
2024-07-14 10:47:58 -0300

    cambio en la direccion de la imagen

--- src/Componentes/Banner/Banner.jsx
+++ src/Componentes/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import Titulo from "../Titulo/Titulo"
 
 const Banner = () => {
     return <section className="banner">
-        <div className="banner__imagen" style={{backgroundImage: `url(./public/Imagenes/banner.png)`}}>
+        <div className="banner__imagen" style={{backgroundImage: `url(https://steamuserimages-a.akamaihd.net/ugc/871874599751199095/8FC99010EE0B683476A25F73EEA7A36281AD37F2/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false)`}}>
         
         </div>
 
